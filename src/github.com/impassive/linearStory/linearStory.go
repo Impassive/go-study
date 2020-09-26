80dac25eee5bcca7c78bbced12372bc343e76d4f
Impassive <tmv1995@gmail.com>
2020-09-26 14:05:57 +0300

    complete lesson 5

--- src/github.com/impassive/linearStory/linearStory.go
+++ src/github.com/impassive/linearStory/linearStory.go
@@ -8,40 +8,38 @@ type storyPage struct {
 	nextPage *storyPage
 }
 
-func playStory(page *storyPage) {
-	if page == nil {
-		return
+//type-specific func - receiver type for func
+func (page *storyPage) playStory() {
+	for page != nil {
+		fmt.Println(page.text)
+		page = page.nextPage
 	}
-	fmt.Println(page.text)
-	playStory(page.nextPage)
 }
 
-//Homework
-func addPage(page *storyPage, text string) storyPage {
-	pageN := storyPage{text, nil}
-	page.nextPage = &pageN
-	return pageN
+func (page *storyPage) addtoEnd(text string) {
+	pageToAdd := &storyPage{text, nil}
+	for page.nextPage != nil {
+		page = page.nextPage
+	}
+	page.nextPage = pageToAdd
 }
 
-func deletePage(page *storyPage, prevPage *storyPage) {
-	prevPage.nextPage = nil
-	page = nil
+func (page *storyPage) addAfter(text string) {
+	newPage := &storyPage{text, page.nextPage}
+	page.nextPage = newPage
 }
+
+// Delete
 
 func main() {
-	//scanner := bufio.NewScanner(os.Stdin)
-
 	page1 := storyPage{"It was a dark and stormy night.", nil}
-	page2 := storyPage{"You are alone, and you need to find the sacred helmet before the bad guys do", nil}
-	page3 := storyPage{"You see a troll ahead", nil}
+	page1.addtoEnd("You are alone, and you need to find the sacred helmet before the bad guys do")
+	page1.addtoEnd("You see a troll ahead")
 
-	page1.nextPage = &page2
-	page2.nextPage = &page3
+	page1.addAfter("Testing AddAfter")
+	page1.playStory()
 
-	page4 := addPage(&page3, "page 4")
-	fmt.Println(&page4)
-
-	deletePage(&page4, &page3)
-	fmt.Println(&page4)
-
+	//functions - has a return value, may execute something
+	//procedures - has no return value, just execute something
+	//methods - functions attached to a struct/object/etc
 }
